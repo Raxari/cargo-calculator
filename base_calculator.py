cda92c135f588b0c7d3c8b9fdfd2fd1fc15ec7c5
Raxari <ivan_a_klimov@icloud.com>
2025-11-30 23:33:13 +0300

    add discount system for heavy cargo

--- base_calculator.py
+++ base_calculator.py
@@ -1,10 +1,15 @@
 class BasePriceCalculator:
     def calculate_base_price(self, weight):
+        # ДРУГИЕ ИЗМЕНЕНИЯ - СИСТЕМА СКИДОК (конфликт!)
         if weight <= 50:
-            return 300
+            price = 300
         elif weight <= 100:
-            return 1000
+            price = 1000
         elif weight <= 300:
-            return 2000
+            price = 2000
+            # НОВОЕ: скидка для тяжелых грузов
+            if weight > 200:
+                price = int(price * 0.9)  # 10% скидка
         else:
             raise ValueError("Вес превышает максимально допустимый")
+        return price
